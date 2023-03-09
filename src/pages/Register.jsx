568e65caad8f475b841242c36eb56739bbeede1a
kajal-2011 <kajalrekha888@gmail.com>
2023-03-09 14:47:49 +0600

    form controller added

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,38 @@
+import { useState } from "react";
+import SectionTitle from "../components/SectionTitle";
+
 const Register = () => {
-  return <div>Register</div>;
+  const [formFields, setFormFields] = useState({
+    name: "",
+    email: "",
+    passwprd: "",
+  });
+  const handleRegister = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="register flex flex-col justify-center items-center">
+      <form onSubmit={handleRegister} className="flex flex-col gap-5">
+        <SectionTitle title={"Register..."} />
+
+        <div className="form-control flex flex-col gap-2">
+          <label htmlFor="name" className="cursor-pointer">
+            Name
+          </label>
+          <input
+            type="text"
+            placeholder="Write your name"
+            id="name"
+            onChange={(e) =>
+              setFormFields({ ...formFields, name: e.target.value })
+            }
+            className="border py-3 px-5 w-[25rem] rounded outline-none focus:border-violet-500 "
+          />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
