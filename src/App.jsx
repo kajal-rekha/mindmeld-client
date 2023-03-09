95d9a19dd690d5ca1732255db1002749a3464fb8
kajal-2011 <kajalrekha888@gmail.com>
2023-03-09 09:50:16 +0600

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Chat from "./pages/Chat";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 const App = () => {
-  return <div className="text-8xl ">hello</div>;
+  return (
+    <div className="app container mx-auto">
+      <div className="upperlevel-contents min-h-screen">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Chat />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="footer text-center py-20 text-gray-500">
+      <p>&copy; {new Date().getFullYear()} Mindmeld. All reserved</p>
+    </footer>
+  );
+};
+
+export default Footer;
